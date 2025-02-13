41c367e7c387459ee7f4079252e1964676bc9c21
Aobakwe Sepeng <aobakwesep@gmail.com>
2025-02-13 17:19:25 +0200

    Added basic form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert('Hello')
+function generatePoem(event) {
+    event.preventDefault();
+
+    alert("Generating poem...");
+
+   
+new Typewriter("#poem", {
+  strings: "Roses are red, violets are blue, I'm a poet, and I know it!",
+  autoStart: true,
+  delay: 1,
+  cursor: "",
+});
+    
+
+}
+
+
+let poemFormElement = document.querySelector("#form");
+poemFormElement.addEventListener("submit", generatePoem);
